427a3c2d3df846010041746ed0b3458d0adc93d6
Grover Trujillo Benites <grover.tb@hotmail.es>
2015-11-21 12:11:34 -0500

    add PersonaNaturalBE

--- app/src/main/java/com/sidet/sidet02/MainActivity.java
+++ app/src/main/java/com/sidet/sidet02/MainActivity.java
@@ -5,26 +5,48 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.sidet.sidet02.BE.PersonaBE;
 import com.sidet.sidet02.BE.PersonaNaturalBE;
 
 
 public class MainActivity extends Activity {
-
+    private EditText txtUserName,txtPassword;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String x = "";
+        //Asociacion OBJECT Java con Objetos XML
+
+        txtUserName = (EditText) findViewById(R.id.txtUserName);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        btnLogin    = (Button) findViewById(R.id.btnLogin);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),txtUserName.getText().toString(),Toast.LENGTH_SHORT).show();
+            // txtUserName.getText();
+                // txtPassword.getText();
+                return;
+            }
+        });
+
+        /*String x = "";
         PersonaBE Usuario= new PersonaBE();
         PersonaBE Usuario2 = new PersonaBE("Grover","Trujillo","Benites",20);
 
         PersonaNaturalBE Usuario3 = new PersonaNaturalBE();
         PersonaNaturalBE Usuario4 = new PersonaNaturalBE("Grover J.","Trujillo","Benites",20,"AV. Sin Nombre","2047012346");
 
-        //Log.i("SIDET:",Usuario4.getNombreCompleto());
+        Log.i("SIDET:",Usuario4.getNombreCompleto());*/
     }
 
     @Override
